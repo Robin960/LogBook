e576c19697798cac6bcc09e324c00892546c5f19
ashishkaul <jnjashishkau@gmail.com>
2018-06-10 02:13:32 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require ('path');
+module.exports = {
+    entry: './index.jsx',
+    output: { 
+       path:  __dirname + '/dist',
+       filename: 'bundle.js'
+    },
+    module : {
+        rules : [
+            {
+                test: /\.jsx?$/,
+                use:'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    resolve : {
+        extensions : ['.js', '.json', '.jsx', '.css']
+    },
+    devtool : 'source-map'
+}
